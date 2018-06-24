b54062196c07351e9b0a7c9ab15af8199f884ee7
Чайка <mellanie335@gmail.com>
2018-06-25 00:56:51 +0900

    Add some constant, enum and extension definition.

--- DeuxCheVaux/WebAPI/OwnerAndVIPCommentHandler.swift
+++ DeuxCheVaux/WebAPI/OwnerAndVIPCommentHandler.swift
@@ -8,6 +8,35 @@
 
 import Cocoa
 
+private let apiBase:String = "http://live2.nicovideo.jp/watch/"
+
+private let operatorComment:String = "/operator_comment"
+private let programExtension:String = "/extension"
+private let vipComment:String = "/bsp_comment"
+private let statistics:String = "/statistics"
+private let contents:String = "/contents"
+private let mixing:String = "/mixing"
+
+private let perm:String = "/perm "
+private let clear:String = "/cls"
+
+enum HTTPMethod:String {
+	case get = "GET"
+	case post = "POST"
+	case put = "PUT"
+	case delete = "DELETE"
+}// end enum httpMehod
+
+enum CommentKeys:String {
+	case comment = "text"
+	case name = "userName"
+	case color = "color"
+	case perm = "isPermanent"
+	case link = "link"
+}// end enum CommentKeys
+
+extension CommentKeys:StringEnum { }
+
 class OwnerAndVIPCommentHandler: NSObject {
 
 }
