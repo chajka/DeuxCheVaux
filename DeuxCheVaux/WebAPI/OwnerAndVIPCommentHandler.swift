6aa5577f6ce81c6b6e3604d793d445b525a2e1e8
Чайка <mellanie335@gmail.com>
2018-06-25 01:52:23 +0900

    Can post owner comment.

--- DeuxCheVaux/WebAPI/OwnerAndVIPCommentHandler.swift
+++ DeuxCheVaux/WebAPI/OwnerAndVIPCommentHandler.swift
@@ -20,6 +20,10 @@ private let mixing:String = "/mixing"
 private let perm:String = "/perm "
 private let clear:String = "/cls"
 
+private let ContentTypeKey:String = "Content-type"
+private let ContentTypeJSON:String = "application/json"
+
+
 enum HTTPMethod:String {
 	case get = "GET"
 	case post = "POST"
@@ -38,5 +42,44 @@ enum CommentKeys:String {
 extension CommentKeys:StringEnum { }
 
 class OwnerAndVIPCommentHandler: NSObject {
+	private let program:String
+	private let apiBaseString:String
+	private let cookies:Array<HTTPCookie>
+	private var request:URLRequest
+	private let session:URLSession
 
-}
+	init(program:String, cookies:Array<HTTPCookie>) {
+		self.program = program
+		self.cookies = cookies
+		apiBaseString = apiBase + self.program
+		session = URLSession(configuration: URLSessionConfiguration.default)
+		let url:URL = URL(string: apiBaseString)!
+		request = URLRequest(url: url)
+		if (cookies.count > 0) {
+			request.allHTTPHeaderFields = HTTPCookie.requestHeaderFields(with: cookies)
+		}// end if have cookies
+	}// end init
+
+	func ownerComment(comment:String, name:String = "", color:String = "", isPerm:Bool = false) throws -> Void {
+		var permanent:Bool = isPerm
+		var commentToPost = String(comment)
+		guard let url = URL(string: apiBaseString + operatorComment) else { return }
+		if (comment.starts(with: perm)) {
+			permanent = true
+			commentToPost = String(comment.suffix(comment.count - perm.count))
+		}// end if comment include permanent command
+		
+		var jsonDict:Dictionary<String, Any> = Dictionary()
+		jsonDict[CommentKeys.comment] = commentToPost
+		if permanent { jsonDict[CommentKeys.perm] = true }
+		if !name.isEmpty { jsonDict[CommentKeys.name] = name }
+		if !color.isEmpty { jsonDict[CommentKeys.color] = color }
+		
+		request.httpBody = try JSONSerialization.data(withJSONObject: jsonDict, options: [])
+		request.url = url
+		request.httpMethod = HTTPMethod.put.rawValue
+		request.addValue(ContentTypeJSON, forHTTPHeaderField: ContentTypeKey)
+		let task:URLSessionDataTask = session.dataTask(with: request)
+		task.resume()
+	}// end func owner comment
+}// end class OwnerAndVIPCommentHandler
---
+++ DeuxCheVauxTests/OwnerAndVIPCommentHandlerTest.swift
@@ -0,0 +1,48 @@
+//
+//  OwnerAndVIPCommentHandlerTest.swift
+//  DeuxCheVauxTests
+//
+//  Created by Чайка on 2018/06/25.
+//  Copyright © 2018 Чайка. All rights reserved.
+//
+
+import XCTest
+
+class OwnerAndVIPCommentHandlerTest: XCTestCase {
+
+	private var cookie:HTTPCookie!
+
+    override func setUp() {
+        super.setUp()
+		cookie = HTTPCookie(properties: [HTTPCookiePropertyKey.name: "user_session", HTTPCookiePropertyKey.value: user_session_value, HTTPCookiePropertyKey.domain: "nicovideo.jp", HTTPCookiePropertyKey.path: "/"])
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func test01_allocation() {
+		let operatorCommentor:OwnerAndVIPCommentHandler = OwnerAndVIPCommentHandler(program:liveNumber, cookies:[cookie])
+		XCTAssertNotNil(operatorCommentor, "operatorCommentor can not initialized")
+    }
+
+	func test02_ownerComment() {
+		let operatorCommentor:OwnerAndVIPCommentHandler = OwnerAndVIPCommentHandler(program:liveNumber, cookies:[cookie])
+		do {
+			try operatorCommentor.ownerComment(comment: "test comment", name: "Чайка", color: "green", isPerm: false)
+			Thread.sleep(forTimeInterval: 10)
+			try operatorCommentor.ownerComment(comment: "/perm test comment", color: "red", isPerm: false)
+		} catch {
+			XCTAssertTrue(false, "JSON serialization throw error")
+		}
+	}
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
