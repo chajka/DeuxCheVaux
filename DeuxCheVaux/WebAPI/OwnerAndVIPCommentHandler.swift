7d3c2129eb04849269553565aa467d52a6f83694
Чайка <mellanie335@gmail.com>
2018-06-25 00:52:38 +0900

    Add empty class OwnerAndVIPCommentHandler into project

---
+++ DeuxCheVaux/WebAPI/OwnerAndVIPCommentHandler.swift
@@ -0,0 +1,13 @@
+//
+//  OwnerAndVIPCommentHandler.swift
+//  DeuxCheVaux
+//
+//  Created by Чайка on 2018/06/25.
+//  Copyright © 2018 Чайка. All rights reserved.
+//
+
+import Cocoa
+
+class OwnerAndVIPCommentHandler: NSObject {
+
+}
