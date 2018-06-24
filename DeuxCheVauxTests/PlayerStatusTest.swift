ceaf5dbd9ae1d8769b9489f9276824222a128e61
Чайка <mellanie335@gmail.com>
2018-06-24 14:49:08 +0900

    parse program number and set to property test OK.

---
+++ DeuxCheVauxTests/PlayerStatusTest.swift
@@ -0,0 +1,44 @@
+//
+//  PlayerStatusTest.swift
+//  DeuxCheVauxTests
+//
+//  Created by Чайка on 2018/06/24.
+//  Copyright © 2018 Чайка. All rights reserved.
+//
+
+import XCTest
+
+class PlayerStatusTest: XCTestCase {
+	private let user_session_value:String = "user_session_6347612_a0c800610aa6bbe0f20ad9edb3ab94d3b8b0b369f96eb0fcfcf70f1ed69e097a"
+	private let liveNumber:String = "lv314021092"
+	
+	private var cookie:HTTPCookie!
+	
+	override func setUp() {
+		super.setUp()
+		cookie = HTTPCookie(properties: [HTTPCookiePropertyKey.name: "user_session", HTTPCookiePropertyKey.value: user_session_value, HTTPCookiePropertyKey.domain: "nicovideo.jp", HTTPCookiePropertyKey.path: "/"])
+	}
+	
+	override func tearDown() {
+		// Put teardown code here. This method is called after the invocation of each test method in the class.
+		super.tearDown()
+	}
+	
+	func test01_allocation() {
+		let playerstatus:PlayerStatus = PlayerStatus(program: liveNumber, cookies: [cookie])
+		XCTAssertNotNil(playerstatus, "player status is nil")
+	}
+	
+	func test02_properties() {
+		let playerstatus:PlayerStatus = PlayerStatus(program: liveNumber, cookies: [cookie])
+		XCTAssertNotNil(playerstatus, "player status is nil")
+		XCTAssertNotNil(playerstatus.programNumber, "property programNumber is nil")
+	}
+	
+	func testPerformanceExample() {
+		// This is an example of a performance test case.
+		self.measure {
+			// Put the code you want to measure the time of here.
+		}
+	}
+}
