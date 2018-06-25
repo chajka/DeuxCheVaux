76b83ffe4a29b329cab929bf1f265fa754792869
Чайка <mellanie335@gmail.com>
2018-06-26 04:50:52 +0900

    Add classes XMLSocketCommentVector and XMLSocketCommentVectorTest

---
+++ DeuxCheVaux/SocketCommunication/XMLSocketCommentVector.swift
@@ -0,0 +1,13 @@
+//
+//  XMLSocketCommentVector.swift
+//  DeuxCheVaux
+//
+//  Created by Чайка on 2018/06/26.
+//  Copyright © 2018 Чайка. All rights reserved.
+//
+
+import Cocoa
+
+class XMLSocketCommentVector: NSObject {
+
+}
