a52ec8cfd50a3b5e28792b69ab87fed588f5f29a
Я Чайка <mellanie335@gmail.com>
2018-09-14 19:39:32 +0900

    Add empty class VideoInformation.

---
+++ DeuxCheVaux/VideoInformation/LegacyInterface/VideoInformation.swift
@@ -0,0 +1,13 @@
+//
+//  VideoInformation.swift
+//  DeuxCheVaux
+//
+//  Created by Я Чайка on 2018/09/14.
+//  Copyright © 2018 Чайка. All rights reserved.
+//
+
+import Cocoa
+
+class VideoInformation: NSObject {
+
+}
