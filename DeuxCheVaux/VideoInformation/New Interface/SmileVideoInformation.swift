0300409345fcf96d721a36c6516b25cc55918010
Я Чайка <mellanie335@gmail.com>
2019-06-07 14:25:30 +0900

    Add new class SmileVideoInformation
    get video information with new interface.

--- DeuxCheVaux/VideoInformation/New Interface/SmileVideoInformation.swift
+++ DeuxCheVaux/VideoInformation/New Interface/SmileVideoInformation.swift
@@ -8,6 +8,106 @@
 
 import Cocoa
 
-class SmileVideoInformation: NSObject {
+private enum VideoInfoTag: String {
+	case title = "title"
+	case description = "description"
+	case time = "length"
+	case view = "view_counter"
+	case commentCount = "comment_num"
+	case mylist = "mylist_counter"
+	case tag = "tag"
+	
+	static func ~= (lhs: VideoInfoTag, rhs: String) -> Bool {
+		return lhs.rawValue ~= rhs ? true : false
+	}// end ~=
+}// end enum VideoInfoTag
 
-}
+internal struct MovieDescription: Codable {
+	let id: String
+	let length: Int
+	let title: String
+	let userId: String
+}// end struct MovieDescription
+
+internal struct MetaInformation: Codable {
+	let status: Int
+	let errorCode: String
+}// end struct MetaInformation
+
+private struct MovieInfo: Codable {
+	let meta: MetaInformation
+	let data: MovieDescription?
+}// end struct MovieInfo
+
+fileprivate let InfoQueryAPI: String = "http://ext.nicovideo.jp/api/getthumbinfo/"
+fileprivate let contentsAPI: String = "http://live2.nicovideo.jp/unama/tool/v1/contents/"
+
+public final class SmileVideoInformation: NSObject {
+	// MARK:   Properties
+	public let videoNumber: String
+	public private(set) var title: String!
+	public private(set) var time: String!
+	public private(set) var videoDescription: String!
+	public private(set) var viewCount: Int!
+	public private(set) var commentCount: Int!
+	public private(set) var myListCount: Int!
+	public private(set) var tags: Array<String>
+	
+	// MARK: - Member variables
+	private let cookies: Array<HTTPCookie>
+	private let session: URLSession
+	private var request: URLRequest!
+	private var stringBuffer: String
+	
+	// MARK: - Constructor/Destructor
+	public init (videoNumber video: String, cookies cookie: Array<HTTPCookie>) {
+		self.videoNumber = video
+		cookies = cookie
+		session = URLSession(configuration: URLSessionConfiguration.default)
+		tags = Array()
+		stringBuffer = String()
+	}// end init
+	
+	// MARK: - Override
+	// MARK: - Actions
+	// MARK: - Public methods\
+	public func parse () -> Bool {
+		let result: Bool = checkContents()
+		
+		return result
+	}// end parse
+	
+	// MARK: - Private methods
+	private func checkContents () -> Bool {
+		if let contentsAPIURL: URL = URL(string: contentsAPI) {
+			let contentsAPIforcurrentVideo = contentsAPIURL.appendingPathComponent(videoNumber)
+			request = URLRequest(url: contentsAPIforcurrentVideo, cachePolicy: URLRequest.CachePolicy.returnCacheDataElseLoad, timeoutInterval: 0.1)
+			request.allHTTPHeaderFields = HTTPCookie.requestHeaderFields(with: cookies)
+			request.method = HTTPMethod.get
+			let semaphore: DispatchSemaphore = DispatchSemaphore(value: 0)
+			let task: URLSessionDataTask = session.dataTask(with: request) { [weak self] (dat, resp, err) in
+				guard let weakSelf = self, let data: Data = dat else { return }
+				do {
+					let description: MovieInfo = try JSONDecoder().decode(MovieInfo.self, from: data)
+					if description.meta.errorCode == "OK" {
+						if let desc: MovieDescription = description.data {
+							weakSelf.title = desc.title
+							let length: String = String(format: "4.2f", Float(desc.length / 60))
+							weakSelf.time = "\(length) min"
+						}// end optional binding check for data
+					}// end if
+				} catch let error {
+					print(error.localizedDescription)
+				}// end do try - catch json serializatioon
+				semaphore.signal()
+			}// end closure
+			task.resume()
+			let timeout: DispatchTimeoutResult = semaphore.wait(timeout: DispatchTime.now() + 1.0)
+			if timeout == DispatchTimeoutResult.success { return true }
+		}// end optional binding check for make contents api base url
+		
+		return false
+	}// end checkContents
+	
+	// MARK: - Delegates
+}// end class VideoInformation
