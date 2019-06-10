9633295ab1b46896822f02911f163a0ebc0de582
Я Чайка <mellanie335@gmail.com>
2019-06-10 13:06:47 +0900

    Define default timeout value.

--- DeuxCheVaux/VideoInformation/LegacyInterface/VideoInformation.swift
+++ DeuxCheVaux/VideoInformation/LegacyInterface/VideoInformation.swift
@@ -19,6 +19,7 @@ private enum VideoInfoTag: String {
 }// end enum VideoInfoTag
 
 private let InfoQueryAPI: String = "http://ext.nicovideo.jp/api/getthumbinfo/"
+private let Timeout: Double = 2.0
 
 public final class VideoInformation: NSObject, XMLParserDelegate {
 		// MARK:   Properties
