afbf18f84187e47847f52144b4a2fbd09ff7332f
Я Чайка <mellanie335@gmail.com>
2019-08-12 06:31:25 +0900

    Implement empty class  NicoNicoInformationFetcher.

---
+++ DeuxCheVaux/WebAPI/NicoNicoInformationFetcher.swift
@@ -0,0 +1,13 @@
+//
+//  NicoNicoInformationFetcher.swift
+//  Charleston
+//
+//  Created by Я Чайка on 2019/08/12.
+//  Copyright © 2019 Чайка. All rights reserved.
+//
+
+import Cocoa
+
+class NicoNicoInformationFetcher: NSObject {
+
+}
