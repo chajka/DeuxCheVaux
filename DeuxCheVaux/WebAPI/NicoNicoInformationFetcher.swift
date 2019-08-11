d603907873be6876443948695bd268016b799d03
Я Чайка <mellanie335@gmail.com>
2019-08-12 06:46:58 +0900

    Add member variable session and configure in initializer.

--- DeuxCheVaux/WebAPI/NicoNicoInformationFetcher.swift
+++ DeuxCheVaux/WebAPI/NicoNicoInformationFetcher.swift
@@ -8,11 +8,23 @@
 
 import Cocoa
 
+private let RequestTimeOut: TimeInterval = 2.0
+private let DataTimeOut: TimeInterval = 2.0
+
 public final class NicoNicoInformationFetcher: NSObject {
 		// MARK:   Outlets
 		// MARK: - Properties
 		// MARK: - Member variables
+	private var session: URLSessionConfiguration
+
 		// MARK: - Constructor/Destructor
+	public override init () {
+		session = URLSessionConfiguration.default
+		session.timeoutIntervalForRequest = RequestTimeOut
+		session.timeoutIntervalForResource = DataTimeOut
+		session.httpCookieAcceptPolicy = HTTPCookie.AcceptPolicy.onlyFromMainDocumentDomain
+	}// end init
+
 		// MARK: - Override
 		// MARK: - Actions
 		// MARK: - Public methods
