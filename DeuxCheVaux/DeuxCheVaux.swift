fe752d194eadae57bf775100dcdf5784343521d6
Я Чайка <mellanie335@gmail.com>
2019-08-12 06:08:42 +0900

    Class DeuxCheVaux become public final.

---
+++ DeuxCheVaux/DeuxCheVaux.swift
@@ -0,0 +1,13 @@
+//
+//  DeuxCheVaux.swift
+//  Charleston
+//
+//  Created by Я Чайка on 2019/08/11.
+//  Copyright © 2019 Чайка. All rights reserved.
+//
+
+import Cocoa
+
+
+public final class DeuxCheVaux: NSObject {
+}// end class DeuxCheVaux
