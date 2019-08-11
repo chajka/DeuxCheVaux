bf7e3c9f52f59d877cf8dd6d4b5149f551885c77
Я Чайка <mellanie335@gmail.com>
2019-08-12 06:10:38 +0900

    Define properties.

--- DeuxCheVaux/DeuxCheVaux.swift
+++ DeuxCheVaux/DeuxCheVaux.swift
@@ -13,6 +13,22 @@ internal let FramewrokName: String = "DeuxCheVaux"
 public final class DeuxCheVaux: NSObject {
 		// MARK:   Class Variable
 		// MARK: - Properties
+	public var applicationName: String
+	public var applicationVersionMajor: Float
+	public var applicationVersionMinor: Int
+	public var applicationVersionFix: Int
+	public var frameworkName: String
+	public var framewrokVersionMajor: Float
+	public var framewrokVersionMinor: Int
+	public var framerokVersionFix: Int
+	public var userAgent: String {
+		get {
+			let applicationNameVersion = "\(applicationName)/\(applicationVersionMajor).\(applicationVersionMinor).\(applicationVersionFix)"
+			let frameworkNameVersiion = "\(framewrokVersionMajor) \(framewrokVersionMajor).\(framewrokVersionMinor).\(framerokVersionFix)"
+			return "\(applicationNameVersion) (\(frameworkNameVersiion))"
+		}// end get
+	}// end computed property
+
 		// MARK: - Member variables
 		// MARK: - Constructor/Destructor
 		// MARK: - Override
