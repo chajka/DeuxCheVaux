f72bec8e48e904a65fb7f5f03d06e70cd51fc86b
Я Чайка <mellanie335@gmail.com>
2019-08-12 08:10:15 +0900

    Implement method fetchNickname from seiga API.

--- DeuxCheVaux/WebAPI/NicoNicoInformationFetcher.swift
+++ DeuxCheVaux/WebAPI/NicoNicoInformationFetcher.swift
@@ -11,6 +11,10 @@ import Cocoa
 fileprivate let RequestTimeOut: TimeInterval = 2.0
 fileprivate let DataTimeOut: TimeInterval = 2.0
 fileprivate let Timeout: Double = 2.0
+fileprivate let NicknameNodeName: String = "nickname"
+fileprivate let CouldNotParse = "Could not parse"
+
+fileprivate let NicknameAPIFormat: String = "http://seiga.nicovideo.jp/api/user/info?id="
 
 public final class NicoNicoInformationFetcher: NSObject {
 		// MARK:   Outlets
@@ -34,6 +38,34 @@ public final class NicoNicoInformationFetcher: NSObject {
 		// MARK: - Public methods
 		// MARK: - Internal methods
 		// MARK: - Private methods
+	private func fetchNickname (fromSeigaAPI identifier: String) -> String? {
+		guard let url = URL(string: NicknameAPIFormat + identifier) else { return nil }
+		let request: URLRequest = makeRequest(url: url, method: .get)
+		let semaphore: DispatchSemaphore = DispatchSemaphore(value: 0)
+		var nickname: String? = nil
+		let task: URLSessionDataTask = session.dataTask(with: request) { (dat: Data?, resp: URLResponse?, err: Error?) in
+			guard let data: Data = dat else {
+				semaphore.signal()
+				return
+			}// end guard
+			do {
+				let seiga: XMLDocument = try XMLDocument(data: data, options: .documentTidyXML)
+				guard let children: Array<XMLNode> = seiga.children?.first?.children?.first?.children else { throw NSError(domain: CouldNotParse, code: 0, userInfo: nil)}
+				for child: XMLNode in children {
+					if child.name == NicknameNodeName { nickname = child.stringValue }
+				}// end foreach children
+			} catch let error {
+				print(error.localizedDescription)
+			}// end do try - catch
+			semaphore.signal()
+		}// end closure
+		task.resume()
+		let timeout: DispatchTimeoutResult = semaphore.wait(timeout: DispatchTime.now() + Timeout)
+		if timeout == .timedOut { nickname = nil }
+
+		return nickname
+	}// end fetchNickname from seiga api
+
 	private func makeRequest (url requestURL: URL, method requestMethod: HTTPMethod, contentsType type: String? = nil) -> URLRequest {
 		let deuxCheVaux: DeuxCheVaux = DeuxCheVaux.shared
 		let userAgent: String = deuxCheVaux.userAgent
