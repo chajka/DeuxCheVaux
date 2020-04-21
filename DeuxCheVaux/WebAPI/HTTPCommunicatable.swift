7889fd057e9307f7a70026184674831e896ba1a6
Я Чайка <mellanie335@gmail.com>
2020-04-22 07:23:44 +0900

    Add new base class HTTPCommunicatable.

---
+++ DeuxCheVaux/WebAPI/HTTPCommunicatable.swift
@@ -0,0 +1,13 @@
+//
+//  HTTPCommunicatable.swift
+//  Charleston
+//
+//  Created by Я Чайка on 2020/04/22.
+//  Copyright © 2020 Чайка. All rights reserved.
+//
+
+import Cocoa
+
+class HTTPCommunicatable: NSObject {
+
+}
