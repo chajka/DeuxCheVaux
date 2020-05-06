72821400371c53f3cd752859dbb40e910de29bcc
Я Чайка <mellanie335@gmail.com>
2020-05-06 10:46:27 +0900

    Implement method for get all NGWords.

--- DeuxCheVaux/WebAPI/OwnerCommandHandler.swift
+++ DeuxCheVaux/WebAPI/OwnerCommandHandler.swift
@@ -582,6 +582,33 @@ public final class OwnerCommandHandler: HTTPCommunicatable {
 		return success
 	}// end addNGWord
 
+	public func allNGWords () -> Array<NGData> {
+		guard let baseURL = URL(string: UserNamaAPITool) else { return Array() }
+		let url = baseURL.appendingPathComponent(program, isDirectory: false).appendingPathComponent(NGWordSetting)
+		var wordsList: Array<NGData> = Array()
+
+		let semaphore: DispatchSemaphore = DispatchSemaphore(value: 0)
+		let request: URLRequest = makeRequest(url: url, method: .get)
+		let task: URLSessionDataTask = session.dataTask(with: request) { [weak self] (dat: Data?, resp: URLResponse?, err: Error?) in
+			defer { semaphore.signal() } // must increment semaphore when exit from closure
+			guard let weakSelf = self, let data: Data = dat else { return }
+			do {
+				let decoder: JSONDecoder = JSONDecoder()
+				let list: NGWrodList = try decoder.decode(NGWrodList.self, from: data)
+				guard .success == weakSelf.checkMetaInformation(list.meta) else { return }
+				for foundWord: NGData in list.data {
+					wordsList.append(foundWord)
+				}// end foreach found word
+			} catch let error {
+				print(error.localizedDescription)
+			}// end do try - catch
+		}// end closure
+		task.resume()
+		_ = semaphore.wait(timeout: DispatchTime.now() + Timeout)
+
+		return wordsList
+	}// end allNGWords
+
 		// MARK: quote
 	public func checkQuotable (_ video: String) -> (quotable: Bool, status: ResultStatus) {
 		guard let url: URL = URL(string: QuatableAPIBase + video) else { return (false, .apiAddressError) }
