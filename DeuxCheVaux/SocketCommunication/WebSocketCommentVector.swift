69dbf390c38a24fab4c86bba113090ac8dd6030e
Я Чайка <mellanie335@gmail.com>
2020-07-15 19:43:44 +0900

    Add new class WebSocketCommentVector.

---
+++ DeuxCheVaux/SocketCommunication/WebSocketCommentVector.swift
@@ -0,0 +1,13 @@
+//
+//  WebSocketCommentVector.swift
+//  Charleston
+//
+//  Created by Я Чайка on 2020/07/15.
+//  Copyright © 2020 Чайка. All rights reserved.
+//
+
+import Cocoa
+
+class WebSocketCommentVector: NSObject {
+
+}// end WebSocketCommentVector
