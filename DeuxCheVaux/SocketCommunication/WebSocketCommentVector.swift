30508321ab76fb2d3a9e101d0eb8c6bd073088d7
Я Чайка <mellanie335@gmail.com>
2020-07-25 11:32:29 +0900

    Implement log message for make  log line of received message.

--- DeuxCheVaux/SocketCommunication/WebSocketCommentVector.swift
+++ DeuxCheVaux/SocketCommunication/WebSocketCommentVector.swift
@@ -100,6 +100,27 @@ public struct ChatElements: Codable {
 	public let score: Int?
 	public let origin: String?
 	public let locale: UserLanguage?
+
+	public func logMessage () -> String {
+		var message: String = String()
+
+		message += "thread:\(thread), "
+		message += "vpos:\(vpos), "
+		message += "no: \(no)"
+		message += "user_id: \(user_id)"
+		message += "no: \(no)"
+		message += "content: \(content)"
+		message += "date: \(date)"
+		message += "date_usec: \(date_usec)"
+		message += "premium: \(String(describing: premium != nil ? premium : 0))"
+		if let mail: String = mail { message += "mail: \(mail)" }
+		if let anonymity: Int = anonymity { message += "anonymity: \(anonymity)" }
+		if let score: Int = score { message += "score: \(score)" }
+		if let origin: String = origin { message += "origin: \(origin)" }
+		if let locale: UserLanguage = locale { message += "locale: \(locale.rawValue)" }
+
+		return message
+	}// end logmesage
 }// end struct ChatElement
 
 struct ChatResult: Codable {
