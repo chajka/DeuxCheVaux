90b2cadf7db75601d2a839177ea4acae4bc4d470
Я Чайка <chajka.niconico@gmail.com>
2021-04-03 06:45:56 +0900

    Add token manager specific files.

---
+++ DeuxCheVaux/NOAuth2/TokenManager.swift
@@ -0,0 +1,19 @@
+//
+//  TokenManager.swift
+//  Charleston
+//
+//  Created by Я Чайка on 2021/03/30.
+//  Copyright © 2021 Чайка. All rights reserved.
+//
+
+import Cocoa
+
+class TokenManager: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+
+}
