1d4c5f3a5786deb021a6a7c990865da0f37d5bd2
Я Чайка <chajka.niconico@gmail.com>
2021-04-03 18:01:42 +0900

    Add new class WebSocketEndpointTalker into target DeuxCheVaux.

---
+++ DeuxCheVaux/SocketCommunication/WebSocketEndpointTalker.swift
@@ -0,0 +1,13 @@
+//
+//  WebSocketEndpointTalker.swift
+//  DeuxCheVaux
+//
+//  Created by Я Чайка on 2021/04/03.
+//  Copyright © 2021 Чайка. All rights reserved.
+//
+
+import Cocoa
+
+class WebSocketEndpointTalker: NSObject {
+
+}// end class WebSocketEndpointTalker
