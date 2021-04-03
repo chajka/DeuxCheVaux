95d375031437c1ef1b723200365a0569a18d7ec7
Я Чайка <chajka.niconico@gmail.com>
2021-04-03 11:53:15 +0900

    Add member variable for user id and nickname.

--- DeuxCheVaux/NOAuth2/TokenManager.swift
+++ DeuxCheVaux/NOAuth2/TokenManager.swift
@@ -59,6 +59,8 @@ public final class TokenManager: NSWindowController, WKNavigationDelegate {
 	private var refreshQuery: String!
 	private var expire: Int = AccessTokenInterval
 	private let session: URLSession = URLSession(configuration: URLSessionConfiguration.default)
+	private var userIdentifier: String!
+	private var userNickname: String!
 
 	private var watcherCount: Int = 0
 
