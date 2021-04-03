3d5008a6bd0b17be8a16ecb28190a8ac045ffa0c
Я Чайка <chajka.niconico@gmail.com>
2021-04-03 20:01:32 +0900

    Define readonly property url and initialize it.

--- DeuxCheVaux/SocketCommunication/WebSocketEndpointTalker.swift
+++ DeuxCheVaux/SocketCommunication/WebSocketEndpointTalker.swift
@@ -15,10 +15,16 @@ public final class WebSocketEndpointTalker: NSObject {
 		// MARK:   Class Variables
 		// MARK: - Class Methods
 		// MARK: - Properties
+	public let url: URL
+
 		// MARK: - Computed Properties
 		// MARK: - Outlets
 		// MARK: - Member Variables
 		// MARK: - Constructor / Destructor
+	public init (url: URL) {
+		self.url = url
+	}// end init
+
 		// MARK: - Overrides
 		// MARK: - Actions
 		// MARK: - Public Methods
