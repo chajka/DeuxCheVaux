b5bc00c4e20fb164ad358e9377b8fad5074d5487
Я Чайка <chajka.niconico@gmail.com>
2021-04-05 14:58:55 +0900

    Do not use sub protocol.

--- DeuxCheVaux/SocketCommunication/WebSocketEndpointTalker.swift
+++ DeuxCheVaux/SocketCommunication/WebSocketEndpointTalker.swift
@@ -9,7 +9,6 @@
 import Cocoa
 import SwiftWebSocket
 
-fileprivate let SubProtocol: String = "msg.nicovideo.jp#json"
 fileprivate let StartWatching: String = "{\"type\":\"startWatching\",\"data\":{\"stream\":{\"quality\":\"abr\",\"limit\":\"super_high\",\"latency\":\"low\",\"chasePlay\":false},\"reconnect\":false}}"
 fileprivate let Pong: String = "{\"type\":\"pong\"}"
 fileprivate let KeepSeat: String = "{\"type\":\"keepSeat\"}"
@@ -95,9 +94,9 @@ public final class WebSocketEndpointTalker: NSObject {
 		var request: URLRequest = URLRequest(url: self.url)
 		request.addValue(DeuxCheVaux.shared.userAgent, forHTTPHeaderField: UserAgentKey)
 		if let runLoop: RunLoop = self.runLoop {
-			endpoint = WebSocket(request: request, subProtocols: [SubProtocol], runLoop: runLoop)
+			endpoint = WebSocket(request: request, runLoop: runLoop)
 		} else {
-			endpoint = WebSocket(request: request, subProtocols: [SubProtocol])
+			endpoint = WebSocket(request: request)
 		}// end optional binding check for runLoop
 		endpoint.compression.on = true
 		endpoint.allowSelfSignedSSL = true
