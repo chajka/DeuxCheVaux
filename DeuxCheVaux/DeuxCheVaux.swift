7a0e01620d6634f98f3f95e458fe36bdef2e4bcb
Я Чайка <chajka.niconico@gmail.com>
2021-04-13 14:59:38 +0900

    Increase framework version.major to 0.4

--- DeuxCheVaux/DeuxCheVaux.swift
+++ DeuxCheVaux/DeuxCheVaux.swift
@@ -9,7 +9,7 @@
 import Cocoa
 
 fileprivate let FrameworkName: String = "DeuxCheVaux"
-fileprivate let FrameworkVersionMajor: Float = 0.3
+fileprivate let FrameworkVersionMajor: Float = 0.4
 fileprivate let FrameworkVersionMinor: Int = 9
 fileprivate let FrameworkVersionFix: Int = 5
 
