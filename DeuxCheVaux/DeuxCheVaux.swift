c9bdd64686c80d27f80193a7e7f13467dc1775bc
Я Чайка <chajka.niconico@gmail.com>
2021-04-20 22:56:13 +0900

    Increase framework version fix to 3.

--- DeuxCheVaux/DeuxCheVaux.swift
+++ DeuxCheVaux/DeuxCheVaux.swift
@@ -11,7 +11,7 @@ import Cocoa
 fileprivate let FrameworkName: String = "DeuxCheVaux"
 fileprivate let FrameworkVersionMajor: Float = 0.4
 fileprivate let FrameworkVersionMinor: Int = 0
-fileprivate let FrameworkVersionFix: Int = 2
+fileprivate let FrameworkVersionFix: Int = 3
 
 fileprivate let QueueLabel: String = "tv.from.chajka.DeuxCheVaux"
 
