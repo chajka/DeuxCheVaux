ef0a4e393344d5a47380136958fd3389ea999965
Чайка <chajka.niconico@gmail.com>
2023-11-19 10:00:00 +0900

    Reset framework version fix to 0.

--- DeuxCheVaux/DeuxCheVaux.swift
+++ DeuxCheVaux/DeuxCheVaux.swift
@@ -11,7 +11,7 @@ import Cocoa
 fileprivate let FrameworkName: String = "DeuxCheVaux"
 fileprivate let FrameworkVersionMajor: Float = 0.5
 fileprivate let FrameworkVersionMinor: Int = 0
-fileprivate let FrameworkVersionFix: Int = 6
+fileprivate let FrameworkVersionFix: Int = 0
 
 fileprivate let QueueLabel: String = "tv.from.chajka.DeuxCheVaux"
 
