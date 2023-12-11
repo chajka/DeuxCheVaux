38d0168adf82567c38210ea3b4565e78e0418d06
Чайка <chajka.niconico@gmail.com>
2023-12-11 11:05:59 +0900

    Increase framework version minor to 2.

--- DeuxCheVaux/DeuxCheVaux.swift
+++ DeuxCheVaux/DeuxCheVaux.swift
@@ -10,7 +10,7 @@ import Cocoa
 
 fileprivate let FrameworkName: String = "DeuxCheVaux"
 fileprivate let FrameworkVersionMajor: Float = 0.6
-fileprivate let FrameworkVersionMinor: Int = 1
+fileprivate let FrameworkVersionMinor: Int = 2
 fileprivate let FrameworkVersionFix: Int = 0
 
 fileprivate let QueueLabel: String = "tv.from.chajka.DeuxCheVaux"
